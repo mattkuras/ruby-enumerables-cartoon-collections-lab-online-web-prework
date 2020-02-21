c13416b4df48640c01aa86b20bd31fb74c5bf751
Matt Kuras <mattkuras@gmail.com>
2020-02-21 20:48:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls)
   calls.any? {|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
